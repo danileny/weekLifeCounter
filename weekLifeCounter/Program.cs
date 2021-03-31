1e6b72a4ddcfc18916d7563581995981b3f0d172
Evgeny D <evgeny_d@outlook.com>
2021-03-31 18:26:23 +0300

    Initial

---
+++ weekLifeCounter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace weekLifeCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите год рождения");
+            int birthYear = int.Parse(Console.ReadLine());
+
+            DateTime thisDay = DateTime.Now;
+            int thisYear = Convert.ToInt32(thisDay.ToString("yyyy"));
+
+            Console.WriteLine("Год рождения: " + birthYear);
+            Console.WriteLine("Текущий год: " + thisYear);
+            Console.WriteLine("Тебе сейчас " + (thisYear - birthYear));           
+
+            Console.WriteLine();
+
+            
+            Console.ReadKey();
+
+            //    System.Threading.Thread.Sleep(300);
+
+
+
+        }
+    }
+}
