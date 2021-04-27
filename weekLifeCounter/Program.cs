b2d29d14bf28d4c7c829c4c3a6430efd4c6e100a
danileny <evgeny_d@outlook.com>
2021-04-27 16:27:01 +0300

    15th

--- weekLifeCounter/Program.cs
+++ weekLifeCounter/Program.cs
@@ -10,6 +10,10 @@ namespace weekLifeCounter
             Console.Write("Введите год рождения: ");
             int birthYear = int.Parse(Console.ReadLine());
 //comment from vsc2
+//comment from vsc2
+//comment from vsc2
+//comment from vsc2
+//comment from vsc2
 
             DateTime thisDay = DateTime.Now;
             int thisYear = Convert.ToInt32(thisDay.ToString("yyyy"));
